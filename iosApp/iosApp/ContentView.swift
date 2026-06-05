3f58542d9f8bbabdd07dd75ffd45733f3775cdc8
Matteo Gaetani <matteogaetani13mg@gmail.com>
2026-06-05 10:55:57 +0200

    Add SwiftUI bounds overlay to debug letterboxing issue

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,9 +3,24 @@ import ComposeApp
 
 struct ContentView: View {
     var body: some View {
-        ComposeView()
-            .ignoresSafeArea(.all)
-            .background(Color.red)
+        GeometryReader { geo in
+            ZStack {
+                ComposeView()
+                    .ignoresSafeArea(.all)
+                    .background(Color.red)
+                
+                VStack {
+                    Text("SwiftUI Bounds: \(Int(geo.size.width)) x \(Int(geo.size.height))")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black.opacity(0.7))
+                        .cornerRadius(8)
+                    Spacer()
+                }
+                .padding(.top, 50)
+            }
+        }
+        .ignoresSafeArea(.all)
     }
 }
 
